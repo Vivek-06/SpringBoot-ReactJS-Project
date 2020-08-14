79948757f7219f90b23aeb3955541a8b77788bd6
Vivek-06 <vivekbehera23@gmail.com>
2020-08-14 07:58:27 +0530

    springboot

--- springrest/src/main/java/com/example/springrest/springrest/entity/Course.java
+++ springrest/src/main/java/com/example/springrest/springrest/entity/Course.java
@@ -1,6 +1,11 @@
 package com.example.springrest.springrest.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Course {
+    @Id
     private long id;
     private String title;
     private String description;
